5e90f13fc7a6481cf0224c11dc32e41ab1fdd172
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-08 07:27:24 +0545

    basic structure of expression classes

---
+++ src/expr.h
@@ -0,0 +1,39 @@
+#ifndef EXPR_H
+#define EXPR_H
+
+#include "token.h"
+#include <variant>
+
+class Visitor {
+	protected:
+		virtual void visit_binary_expr() = 0;
+		virtual void visit_unary_expr() = 0;
+		virtual void visit_grouping_expr() = 0;
+		virtual void visit_literal_expr() = 0;
+};
+
+class Expr {
+	protected:
+		virtual void accept(Visitor)=0;
+};
+
+class Binary: Expr {
+	Expr* left;
+	Token _operator;
+	Expr* right;
+};
+
+class Grouping: Expr {
+	Expr* expr;
+};
+
+class Literal: Expr {
+	std::variant<std::monostate,double> value;
+};
+
+class Unary: Expr {
+	Token _operator;
+	Expr* right;
+};
+
+#endif
