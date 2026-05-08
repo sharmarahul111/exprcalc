5d806be88cb26f57c831ae3e9113ddc6588bfbfb
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-08 11:25:14 +0545

    primary expression parser complete

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,7 +1,35 @@
 #include "parser.h"
+#include "expr.h"
 #include "token.h"
 Expr* Parser::parse(){
-	return expression();
+		return expression();
+}
+Expr* Parser::expression(){
+	return term();
+}
+Expr* Parser::term(){
+	return factor();
+}
+Expr* Parser::factor(){
+	return unary();
+}
+Expr* Parser::unary(){
+	return primary();
+}
+
+Expr* Parser::primary(){
+	if (match(TOKEN_NUMBER)) {
+		double value = std::get<double>(previous().literal);
+		return new Literal(value);
+	} else if (match(TOKEN_IDENTIFIER)) {
+		// check if it's constant or function call
+	} else if (match(TOKEN_LEFT_PAREN)) {
+		Expr* expr = expression();
+		consume(TOKEN_RIGHT_PAREN, "No closing ) found.");
+		return new Grouping(expr);
+	}
+	// throw expected expression
+	throw ParseError(peek(), "Expected expression.");
 }
 
 bool Parser::match(TokenType t){
@@ -28,4 +56,10 @@ Token Parser::advance(){
 }
 Token Parser::previous(){
 	return tokens[current - 1];
+}
+Token Parser::consume(TokenType t, std::string_view msg){
+	if (match(t)) {
+		advance();
+	}
+	throw ParseError(peek(), msg);
 }
