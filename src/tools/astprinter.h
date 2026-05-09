5f13ddc23f5e7fb096c707f6b2fab43abdd87e19
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-09 18:46:54 +0545

    ast printer prototype

--- src/tools/astprinter.h
+++ src/tools/astprinter.h
@@ -1,4 +1,22 @@
+#ifndef ASTPRINTER_H
+#define ASTPRINTER_H
+
 #include<iostream>
+#include<string>
+#include<vector>
 #include "../token.h"
 #include "../expr.h"
+using Exprs = std::vector<Expr*>;
+class ASTPrinter: public Visitor {
+	private:
+		Value parenthesize(std::string, Exprs);
+		Value visit_binary_expr(Binary&);
+		Value visit_grouping_expr(Grouping&);
+		Value visit_literal_expr(Literal&);
+		Value visit_unary_expr(Unary&);
+	public:
+		std::string print(Expr*);
+};
 void printExpr(Expr* exp);
+
+#endif
