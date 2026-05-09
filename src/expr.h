26e5d0653bcf3676a1a01c8cc84d7a2016f19c66
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-09 18:45:42 +0545

    multi type value return from visitors

--- src/expr.h
+++ src/expr.h
@@ -1,25 +1,26 @@
 #ifndef EXPR_H
 #define EXPR_H
 
-#include "token.h"
 #include <variant>
+#include<string>
+#include "token.h"
 class Binary;
 class Unary;
 class Literal;
 class Grouping;
 
+using Value = std::variant<std::monostate, std::string, double>;
 class Visitor {
 	public:
-		virtual void visit_binary_expr(Binary&) = 0;
-		virtual void visit_unary_expr(Unary&) = 0;
-		virtual void visit_grouping_expr(Grouping&) = 0;
-		virtual void visit_literal_expr(Literal&) = 0;
+		virtual Value visit_binary_expr(Binary&) = 0;
+		virtual Value visit_unary_expr(Unary&) = 0;
+		virtual Value visit_grouping_expr(Grouping&) = 0;
+		virtual Value visit_literal_expr(Literal&) = 0;
 };
 
 class Expr {
-	protected:
-		virtual void accept(Visitor*)=0;
 	public:
+		virtual Value accept(Visitor*)=0;
 		virtual ~Expr(){};
 };
 
@@ -28,7 +29,7 @@ class Binary: public Expr {
 		Expr* left {nullptr};
 		Token _operator;
 		Expr* right {nullptr};
-		void accept(Visitor*) override;
+		Value accept(Visitor*) override;
 		Binary(Token _operator, Expr *left, Expr*right){
 			this->_operator = _operator;
 			this->left = left;
@@ -45,7 +46,7 @@ class Binary: public Expr {
 class Grouping: public Expr {
 	public:
 		Expr* expr {nullptr};
-		void accept(Visitor*) override;
+		Value accept(Visitor*) override;
 		Grouping(Expr *expr){
 			this->expr = expr;
 		}
@@ -58,7 +59,7 @@ class Grouping: public Expr {
 class Literal: public Expr {
 	public:
 		std::variant<std::monostate,double> value;
-		void accept(Visitor*) override;
+		Value accept(Visitor*) override;
 		Literal(double v){
 			value = v;
 		}
@@ -76,7 +77,7 @@ class Unary: public Expr {
 			delete right;
 			right = nullptr;
 		}
-		void accept(Visitor*) override;
+		Value accept(Visitor*) override;
 };
 
 #endif
